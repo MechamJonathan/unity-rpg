48d89a885b31e33b06e6194d43d1e0d18358257c
MechamJonathan <jonsmec95@gmail.com>
2025-10-06 06:12:25 -0600

    jump attack

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -18,6 +18,7 @@ public class Player : MonoBehaviour
     public Player_WallJumpState wallJumpState { get; private set; }
     public Player_DashState dashState { get; private set; }
     public Player_BasicAttackState basicAttackState { get; private set; }
+    public Player_JumpAttackState jumpAttackState { get; private set; }
 
 
     [Header("Attack details")]
@@ -69,6 +70,7 @@ public class Player : MonoBehaviour
         wallJumpState = new Player_WallJumpState(this, stateMachine, "jumpFall");
         dashState = new Player_DashState(this, stateMachine, "dash");
         basicAttackState = new Player_BasicAttackState(this, stateMachine, "basicAttack");
+        jumpAttackState = new Player_JumpAttackState(this, stateMachine, "jumpAttack");
     }
 
     private void OnEnable()
