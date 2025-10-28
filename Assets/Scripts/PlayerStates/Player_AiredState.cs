cec422cebe1e04ba266f39ae859a10a4e4e46a13
MechamJonathan <jonsmec95@gmail.com>
2025-10-28 05:36:41 -0600

    update aired state

--- Assets/Scripts/PlayerStates/Player_AiredState.cs
+++ Assets/Scripts/PlayerStates/Player_AiredState.cs
@@ -10,7 +10,6 @@ public class Player_AiredState : EntityState
     public override void Update()
     {
         base.Update();
-
         if (player.moveInput.x != 0)
             player.SetVelocity(player.moveInput.x * (player.moveSpeed * player.inAirMoveMultiplier), rb.linearVelocity.y);
 
